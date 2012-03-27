1ac324d5ab50e989146c1488a5cbfeb5bd3014f1
Roberto Araujo <roberto.andrioli@gmail.com>
2012-03-27 16:57:52 -0300

    initial implementation

--- src/CircularArrayList.java
+++ src/CircularArrayList.java
@@ -8,17 +8,66 @@ public class CircularArrayList<E> extends AbstractList<E>
 
 	private static final long serialVersionUID = -4886139346193901717L;
 
+	private transient Object[] elementData;
+
+	private int size;
+
+	private int head;
+
+	private int tail;
+
+	public CircularArrayList(int initialCapacity) {
+		super();
+		if (initialCapacity < 0) {
+			throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
+		}
+		head = tail = 0;
+		elementData = new Object[initialCapacity];
+	}
+
 	public CircularArrayList() {
+		this(10);
 	}
 
 	@Override
 	public int size() {
-		throw new UnsupportedOperationException("Not implemented yet");
+		return size;
 	}
 
 	@Override
-	public E get(int index) {
-		throw new UnsupportedOperationException("Not implemented yet");
+	public boolean isEmpty() {
+		return size == 0;
 	}
+
+	@Override
+	public boolean add(E e) {
+		ensureCapacity(size + 1);
+		elementData[tail++] = e;
+		size++;
+		return true;
+	}
+
+	private void ensureCapacity(int minCapacity) {
+		int oldCapacity = elementData.length;
+
+		if (minCapacity > oldCapacity) {
+			Object[] oldData = elementData;
+			int newCapacity = (oldCapacity * 3)/2 + 1;
+			if (newCapacity < minCapacity) {
+				newCapacity = minCapacity;
+			}
+			elementData = new Object[newCapacity];
+			System.arraycopy(oldData, 0, elementData, 0, tail);
+			System.arraycopy(oldData, tail, elementData, elementData.length + head, -head);
+		}
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public E get(int index) {
+		index = ((elementData.length + head) + index) % elementData.length;
+		return (E) elementData[index];
+	}
+
 }
 
